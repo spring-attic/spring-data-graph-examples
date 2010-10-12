1c4491de1a1b1720333568d6930f24e7b81cc925
Thomas Risberg <trisberg@vmware.com>
2010-10-11 23:16:19 -0400

    added IMDB roo script and roo startup scripts

---
+++ roo-imdb/fix-persistence-xml.sh
@@ -0,0 +1,5 @@
+sed '/\<\/provider>/ a\ 
+\        \<class>org.neo4j.examples.imdb.domain.Actor\<\/class> \
+\        \<class>org.neo4j.examples.imdb.domain.Movie\<\/class> \
+\        \<class>org.neo4j.examples.imdb.domain.Role\<\/class>
+' 
